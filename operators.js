5c2b785c7798b3e043b7f91aa367c8e4490a1686
Bhavesh Rathod <hbhavesr@amazon.com>
2025-12-11 11:00:48 +0530

    added comparision code

--- operators.js
+++ operators.js
@@ -10,4 +10,63 @@ console.log(`Division of ${a} and ${b} is : ${a / b}`)
 console.log(`Modulus of ${a} and ${b} is : ${a % b}`)
 
 console.log("\nAssignment Operators:")
+let c = 10
+c +=10 // c = c + c
+console.log(`Print c : ${c}`)
+
+c -=c  // c = c - c
+console.log(`Print c : ${c}`)
+
+
+console.log("Comparision Operator")
+
+let value1 = 1
+let value2 ="1"
+
+console.log("Compare using == operator", value1 ==value2) // Compares values, ignores datatype true (1 == "1")
+console.log("compare using the === operator", value1===value2) //Compares values and datatype. false (number ≠ string)
+console.log("compare using the !=", value1 != value2) //Checks if values are not equal (datatype ignored). false (1 == "1")
+
+
+console.log("Logical operators")
+
+/**Logical operators are extremely important in automation because we use them in:
+
+If conditions
+
+Validation statements
+
+Assertions
+
+Loop conditions
+
+Page state checks
+
+API response checks */
+
+
+console.log("Logical operator &&")
+let age = 19
+let eligibleForLicence = true
+
+if ( age >= 20 && eligibleForLicence){
+ console.log ("Eligible for the licence")
+}
+else
+{
+    console.log("Wait for till you grow up ")
+}
+
+console.log("logical operator ||")
+
+let isIndianCitizen = false
+let isAge = 15
+
+if ( isAge == 14 || isIndianCitizen == true) {
+    console.log("You are eligible for apply visa for the Italy")
+}
+else
+{
+    console.log("Wait for sometime dear for being eligible")
+}
 
