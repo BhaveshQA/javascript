83f88d2cdffc6c663afe319a1f6a449b5a5ecdab
Bhavesh Rathod <hbhavesr@amazon.com>
2025-12-12 13:04:11 +0530

    Added conditionl.js

---
+++ conditional.js
@@ -0,0 +1,22 @@
+console.log("If conditional statement")
+
+console.log("Simple IF")
+console.log("------------------")
+let tempreture1 = 31
+
+if(tempreture1 >=30){
+    console.log("Alexa turn on Ac in all rooms")
+}
+
+
+console.log("-----------------------------------")
+console.log("Simple If else statement")
+let tempreture2 = 29
+
+if(tempreture2 >=30){
+    console.log("Alexa Turn on Ac in all rooms")
+}
+else
+{
+    console.log("Tempreture is not that much hot....")
+}
