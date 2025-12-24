1ed9f3cf35ce0ce2ad383644baa4ffe86fd07659
Bhavesh Rathod <hbhavesr@amazon.com>
2025-12-24 15:14:05 +0530

    practice of return statement in function

--- function.js
+++ function.js
@@ -63,3 +63,137 @@ function print2Table(){
     }
 }
 print2Table()
+
+console.log("---------------------------------")
+
+/**
+ * Example: Print numbers divisible by N (loop + param)
+ */
+
+function divisionByNumber(number, division){
+
+    for(let i =1; i<=number; i++){
+        if(i%division===0){
+            console.log(`Print the number ${i}`)
+        }
+    }
+}
+
+divisionByNumber(10,2)
+console.log("------------------------------------")
+
+/**
+ * Example : Validate age for ticket
+ */
+
+console.log("Validate age for ticket")
+
+function ageCheker(age){
+    if(age < 5){
+        console.log("free ticket")
+    }
+   else if(age >= 5 && age < 15){
+        console.log("Ticket price is 500")
+    }
+    else if(age>=15 && age <= 50){
+        console.log("Ticket price is 1500")
+    }
+    else{
+        console.log("Ticket price is 700")
+    }
+}
+
+ageCheker(5)
+
+
+console.log("------------------------------")
+
+/**
+ * ADVANCED FUNCTIONS (Return + Automation Thinking)
+ */
+
+console.log("Return even count (loop + return)")
+
+console.log("Basic Return via function")
+
+function maxNumber(a, b){
+
+    if(a>b){
+        return a
+    }
+    else
+    {
+        return b
+    }
+}
+
+console.log(maxNumber(10,20))
+
+
+/**
+ * Return odd or even
+ */
+
+console.log("Return odd or even")
+
+function oddEven(number){
+
+    if(number%2===0){
+        return "even"
+    }
+    return "odd"
+}
+
+console.log(` number is ${oddEven(5)}`)
+
+/**
+ * return boolean value
+ */
+
+console.log("Return boolean value")
+
+function booleanValue(i){
+   
+    return i > 5
+}
+
+console.log(booleanValue(5))
+
+
+console.log("--------------------------")
+
+/**
+ * Example 1: Find maximum of 3 numbers
+ */
+
+function findMaxNumber(a,b,c){
+    if( a> b && a > c)
+        return a
+    if(b > a && b > c)
+        return b
+    return c
+}
+
+console.log(findMaxNumber(9,10,7))
+
+console.log("---------------------")
+
+/**
+ * Count even numbers (loop)
+ */
+
+console.log("Count even numbers (loop)")
+
+function countEvenNumber(n){
+    let count = 0
+
+    for(let i = 1; i<=n; i++){
+        if(i%2===0){
+            count++
+        }
+
+    }
+    return count
+}
+
+console.log(countEvenNumber(10))
