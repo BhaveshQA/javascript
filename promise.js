7fa5fbcc404ea1b53b28014bccb747ae794276a9
Bhavesh Rathod <hbhavesr@amazon.com>
2026-01-09 09:49:14 +0530

    first program with promise

--- promise.js
+++ promise.js
@@ -9,3 +9,25 @@
 console.log("Start of the execution")
 setTimeout(()=>{console.log("wait for 1 sec")}, 1000);
 console.log("End of the Execution")
+
+
+/**
+ * promise: A value that will be available later. This is used for the solution of aysyncronous flow
+ * it has 3 states : pending, fullfilled, rejected
+ * 
+ */
+
+
+const myPromise = new Promise((resolve, rejected) => {
+    let sucess = true
+    if(sucess){
+        resolve("Promise resolved")
+    }
+    else{
+        rejected("Promise rejected")
+    }
+});
+
+myPromise
+.then(result => console.log(result))
+.catch(error => console.log(error))
