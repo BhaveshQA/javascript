326abfca9059c4303975e79ae016f40df6c5bd89
Bhavesh Rathod <hbhavesr@amazon.com>
2026-01-11 22:50:42 +0530

    added promise example

--- promise.js
+++ promise.js
@@ -31,3 +31,61 @@ const myPromise = new Promise((resolve, rejected) => {
 myPromise
 .then(result => console.log(result))
 .catch(error => console.log(error))
+
+
+/**
+ * Basic Promise with delay (Success)
+ */
+
+console.log("----------------------------")
+const orderFood = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve("Delay with 2 sec")
+    },2000)
+})
+
+orderFood.then((result)=>console.log(result))
+
+
+/**
+ * Promise with Failure (reject)
+ */
+
+console.log("------------------------------")
+
+const paymentProcess = new Promise((resolve, reject) => {
+    let paymentStatus = false
+    if(paymentStatus){
+        resolve("Payment success")
+    }
+    else
+    {
+        reject("Payment failed")
+    }
+})
+
+paymentProcess.then((result)=>{
+    console.log(result)
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+
+/**
+ * Promise chaining (then → then)
+ * Each .then() returns a new promise.
+Whatever you return from one .then() becomes the input to the next .then().
+ */
+
+const userRecords = new Promise((resolve)=>{
+    resolve("Get user data")
+})
+
+userRecords.then((login)=>{
+    console.log(login)
+    return "Get profile information"
+})
+.then((profile)=>{
+    console.log(profile)
+})
